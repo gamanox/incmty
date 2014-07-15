0e370f968879ebc245c30249f376390ecbc52eb6
Will Alvarez <noxwill@gmail.com>
2014-07-15 15:55:33 -0500

    correct profilepic size and request acces to email

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :facebook, '261219254067038', '9e1d273a026b2322dabe832e04c719da'
+  provider :facebook, '261219254067038', '9e1d273a026b2322dabe832e04c719da', :scope => 'email,user_birthday,read_stream', :display => 'popup', :image_size => 'large'
 
 end
