b7b3174fce4fa5b245a4cbec3eec001a37050d34
Will Alvarez <noxwill@gmail.com>
2014-08-28 13:38:23 -0500

    share

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,22 +37,23 @@
 
 $(function(){
   $('#facebtn-cont').on('click', function() {
-    // return FB.api('/me/feed', 'post', {
-    //   message: 'Hello, world!'
-    // }, function(response) {
-    //   if (!response || response.error) {
-    //     return alert("Error occured");
-    //   } else {
-    //     return alert("Action was successful! Action ID: " + response.id);
-    //   }
-    // });
-  FB.ui({
-    method: 'share',
-    href: 'https://incmty.herokuapp.com/',
-    caption: 'prueba',
-    description: 'quiz del emprendedor',
-    picture: 'https://incmty.herokuapp.com/her-mastermind.jpg'
-  }, function(response){});
+    return FB.api('/me/feed', 'post', {
+      message: 'Hello, world!',
+      picture: 'https://incmty.herokuapp.com/her-mastermind.jpg'
+    }, function(response) {
+      if (!response || response.error) {
+        return alert("Error occured");
+      } else {
+        return alert("Action was successful! Action ID: " + response.id);
+      }
+    });
+  // FB.ui({
+  //   method: 'share',
+  //   href: 'https://incmty.herokuapp.com/',
+  //   caption: 'prueba',
+  //   description: 'quiz del emprendedor',
+  //   picture: 'https://incmty.herokuapp.com/her-mastermind.jpg'
+  // }, function(response){});
   });
 });
 
