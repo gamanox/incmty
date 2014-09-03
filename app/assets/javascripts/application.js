13ca0f38512c133cfdf0cf757e534da560278a7b
Will Alvarez <noxwill@gmail.com>
2014-09-03 14:32:53 -0500

    ga

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,3 +50,67 @@ var userData = {
 var sortprofile = [];
 
 
+var gaua, pageView;
+
+gaua = 'UA-42769061-4';
+
+pageView = function(page) {
+  console.log('analytics: ' + page);
+  return ga('send', 'pageview', '/' + page);
+};
+
+(function(i, s, o, g, r, a, m) {
+  i["GoogleAnalyticsObject"] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments);
+  };
+  i[r].l = 1 * new Date();
+  a = s.createElement(o);
+  m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+
+ga("create", gaua, "auto");
+
+ga("send", "pageview");
+
+$(function() {
+  $('#help').on('click', function() {
+    return pageView('faqs');
+  });
+  $('#todosbtn-cont').on('click', function() {
+    return pageView('todos-personajes');
+  });
+  $('#sharebtn-cont').on('click', function() {
+    return pageView('abrir-compartir');
+  });
+  $('#repeatbtn').on('click', function(e) {
+    return pageView('repetirquiz');
+  });
+  $('.case1-next').on('click', function() {
+    return pageView('caso2');
+  });
+  $('.case2-next').on('click', function() {
+    return pageView('caso3');
+  });
+  $('.case3-next').on('click', function() {
+    return pageView('caso4');
+  });
+  $('.case4-next').on('click', function() {
+    return pageView('caso5');
+  });
+  $('.case5-next').on('click', function() {
+    return pageView('caso6');
+  });
+  $('.case6-next').on('click', function() {
+    return pageView('resultado');
+  });
+  $('#facebtn-cont').on('click', function() {
+    return pageView('compartir-facebook');
+  });
+  return $('#twitbtn-cont').on('click', function() {
+    return pageView('compartir-twitter');
+  });
+});
