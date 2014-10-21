3437accd104b11efeb49a8bdb6071407ac57729b
Will Alvarez <noxwill@gmail.com>
2014-10-20 19:37:51 -0500

    certificate

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,6 +1,6 @@
 class QuizController < ApplicationController
   # protect_from_forgery with: :exception
-  
+  after_action :allow_iframe, only: :embed  
   def update_user
     
     user = User.from_omniauth(env["omniauth.auth"])
@@ -16,7 +16,9 @@ class QuizController < ApplicationController
     end
 
 
-
+def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
   helper_method :current_user  
 
 end
